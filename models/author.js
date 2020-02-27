d67438c816f9a66e33c31953e2df815ea88edc97
Kern Elder <kelel1@yahoo.com>
2020-02-26 21:11:02 -0500

    add bookinstance.js / genre.js schemas

--- models/author.js
+++ models/author.js
@@ -9,7 +9,7 @@ const AuthorSchema= new Schema(
       date_of_birth: {type: Date},
       date_of_death: {type: Date},
     }
-  );
+  )
   
   // Virtual for author's full name
   AuthorSchema
@@ -19,30 +19,30 @@ const AuthorSchema= new Schema(
   // To avoid errors in cases where an author does not have either a family name or first name
   // We want to make sure we handle the exception by returning an empty string for that case
   
-    var fullname = '';
+    var fullname = ''
     if (this.first_name && this.family_name) {
       fullname = this.family_name + ', ' + this.first_name
     }
     if (!this.first_name || !this.family_name) {
-      fullname = '';
+      fullname = ''
     }
   
-    return fullname;
-  });
+    return fullname
+  })
   
   // Virtual for author's lifespan
   AuthorSchema
   .virtual('lifespan')
   .get(function () {
-    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
-  });
+    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString()
+  })
   
   // Virtual for author's URL
   AuthorSchema
   .virtual('url')
   .get(function () {
-    return '/catalog/author/' + this._id;
-  });
+    return '/catalog/author/' + this._id
+  })
   
   //Export model
-  module.exports = mongoose.model('Author', AuthorSchema);
+  module.exports = mongoose.model('Author', AuthorSchema)
--- models/book.js
+++ models/book.js
@@ -10,14 +10,14 @@ const BookSchema = new Schema(
       isbn: {type: String, required: true},
       genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
     }
-  );
+  )
   
   // Virtual for book's URL
   BookSchema
   .virtual('url')
   .get(function () {
-    return '/catalog/book/' + this._id;
-  });
+    return '/catalog/book/' + this._id
+  })
   
   //Export model
   module.exports = mongoose.model('Book', BookSchema)
