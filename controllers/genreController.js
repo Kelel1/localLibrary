b1f7eb3f05b16d692382e1619f7c1e1505b92753
Kern Elder <kelel1@yahoo.com>
2020-04-03 16:37:39 -0400

    implement display for genre update form on GET

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -154,8 +154,17 @@ exports.genre_delete_post = function(req, res, next) {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update GET');
+exports.genre_update_get = function(req, res, next) {
+    
+    Genre.findById(req.params.id, function(err, genre) {
+      if (err) { return next(err); }
+      if (genre == null) { // No results
+        var err = new Error('Genre not found');
+        err.status = 404;
+        return next(err); 
+      }
+      res.render('genre_form', { title: 'Update Genre', genre: genre })
+    });
 };
 
 // Handle Genre update on POST.
